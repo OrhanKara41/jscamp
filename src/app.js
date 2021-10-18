6f3473de63625889d6c32ad5615f9560657074ff
Orhan Kara <okara@bimser.com.tr>
2021-10-19 00:02:15 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,26 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun=9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+
+console.log(euroDun)
+
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for (let index = 0; index < konutKredileri.length; index++) {
+    console.log("<li>"+konutKredileri[index]+"</li>")
+    
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
